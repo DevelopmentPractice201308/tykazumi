001e89e576dd5889fa572ede1e7c617c7156b82c
tomomi yabiku <your.tomomi_y-11a@stu.kbc.ac.jp>
2013-09-13 09:31:58 +0900

    tempolary commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 SampleApp::Application.routes.draw do
   resources :users
-
+  resources :sessions, only: [:new, :create, :destroy]
 
  root to: 'static_pages#home'
 
@@ -8,7 +8,9 @@ SampleApp::Application.routes.draw do
   # get "static_pages/help"
 
   
- match '/signup', to: 'users#new'
+  match '/signup', to: 'users#new'
+  match '/signin',  to: 'sessions#new'
+  match '/signout', to: 'sessions#destroy', via: :delete
 
   match "/help", to: 'static_pages#help'
   match "/about", to: 'static_pages#about'
